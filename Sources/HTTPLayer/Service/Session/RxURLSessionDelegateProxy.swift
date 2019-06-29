d407a6c76771e80e28e5f9b85f1ec2719eecabb9
Marek Kojder <marek.kojder@xsolve.pl>
2019-06-29 15:32:59 +0200

    Started using rx URLSession. Work in progress.

--- Sources/HTTPLayer/Service/Session/RxURLSessionDelegateProxy.swift
+++ Sources/HTTPLayer/Service/Session/RxURLSessionDelegateProxy.swift
@@ -12,10 +12,62 @@ typealias RxURLSessionDelegate = URLSessionDataDelegate & URLSessionDownloadDele
 
 class RxURLSession: NSObject {
     private let configuration: URLSessionConfiguration
-    private(set) var urlSession: URLSession?
+    private var urlSession: URLSession?
 
     init(configuration: URLSessionConfiguration) {
         self.configuration = configuration
+    }
+
+    ///Cancels all outstanding tasks and then invalidates the session.
+    func invalidateAndCancel() {
+        urlSession?.invalidateAndCancel()
+    }
+
+    /**
+     Creates a task that retrieves the contents of a URL based on the specified URL request object.
+
+     - Parameter request: A URL request object that provides request-specific information such as the URL, cache policy, request type, and body data or body stream.
+
+     - Returns: The new session data task.
+
+     By creating a task based on a request object, you can tune various aspects of the task’s behavior, including the cache policy and timeout interval.
+
+     After you create the task, you must start it by calling its resume() method.
+     */
+    func dataTask(with request: URLRequest) -> URLSessionDataTask? {
+        return urlSession?.dataTask(with: request)
+    }
+
+    /**
+     Creates a task that performs an HTTP request for uploading the specified file.
+
+     - Parameters:
+       - request: A URL request object that provides the URL, cache policy, request type, and so on. The body stream and body data in this request object are ignored.
+       - fileURL: The URL of the file to upload.
+
+     - Returns: The new session upload task.
+
+     An HTTP upload request is any request that contains a request body, such as a `POST` or `PUT` request. Upload tasks require you to create a request object so that you can provide metadata for the upload, like HTTP request headers.
+
+     After you create the task, you must start it by calling its resume() method. The task calls methods on the session’s delegate to provide you with the upload’s progress, response metadata, response data, and so on.
+     */
+    func uploadTask(with request: URLRequest, fromFile fileURL: URL) -> URLSessionUploadTask? {
+        return urlSession?.uploadTask(with: request, fromFile: fileURL)
+    }
+
+    /**
+     Creates a download task that retrieves the contents of a URL based on the specified URL request object and saves the results to a file.
+
+     - Parameter request: A URL request object that provides the URL, cache policy, request type, body data or body stream, and so on.
+
+     - Returns: The new session download task.
+
+     By creating a task based on a request object, you can tune various aspects of the task’s behavior, including the cache policy and timeout interval.
+
+     After you create the task, you must start it by calling its resume() method. The task calls methods on the session’s delegate to provide you with progress notifications, the location of the resulting temporary file, and so on.
+     */
+    func downloadTask(with request: URLRequest) -> URLSessionDownloadTask? {
+        return urlSession?.downloadTask(with: request)
     }
 }
 
@@ -88,9 +140,9 @@ extension Reactive where Base: RxURLSession {
         return delegate.methodInvoked(#selector(RxURLSessionDelegate.urlSession(_:task:didSendBodyData:totalBytesSent:totalBytesExpectedToSend:))).map { parameters in
             return (parameters[0] as! URLSession,
                     parameters[1] as! URLSessionTask,
-                    parameters[1] as! Int64,
-                    parameters[1] as! Int64,
-                    parameters[1] as! Int64)
+                    parameters[2] as! Int64,
+                    parameters[3] as! Int64,
+                    parameters[4] as! Int64)
         }
     }
 
@@ -98,7 +150,7 @@ extension Reactive where Base: RxURLSession {
         return delegate.methodInvoked(#selector(RxURLSessionDelegate.urlSession(_:task:didCompleteWithError:))).map { parameters in
             return (parameters[0] as! URLSession,
                     parameters[1] as! URLSessionTask,
-                    parameters[1] as? Error)
+                    parameters[2] as? Error)
         }
     }
 
@@ -107,8 +159,8 @@ extension Reactive where Base: RxURLSession {
         return delegate.methodInvoked(#selector(RxURLSessionDelegate.urlSession(_:dataTask:didReceive:completionHandler:))).map { parameters in
             return (parameters[0] as! URLSession,
                     parameters[1] as! URLSessionDataTask,
-                    parameters[1] as! URLResponse,
-                    parameters[1] as! (URLSession.ResponseDisposition) -> Void)
+                    parameters[2] as! URLResponse,
+                    parameters[3] as! (URLSession.ResponseDisposition) -> Void)
         }
     }
 
@@ -116,7 +168,7 @@ extension Reactive where Base: RxURLSession {
         return delegate.methodInvoked(#selector(RxURLSessionDelegate.urlSession(_:dataTask:didReceive:))).map { parameters in
             return (parameters[0] as! URLSession,
                     parameters[1] as! URLSessionDataTask,
-                    parameters[1] as! Data)
+                    parameters[2] as! Data)
         }
     }
 
@@ -125,7 +177,7 @@ extension Reactive where Base: RxURLSession {
         return delegate.methodInvoked(#selector(RxURLSessionDelegate.urlSession(_:downloadTask:didFinishDownloadingTo:))).map { parameters in
             return (parameters[0] as! URLSession,
                     parameters[1] as! URLSessionDownloadTask,
-                    parameters[1] as! URL)
+                    parameters[2] as! URL)
         }
     }
 
@@ -133,9 +185,9 @@ extension Reactive where Base: RxURLSession {
         return delegate.methodInvoked(#selector(RxURLSessionDelegate.urlSession(_:downloadTask:didWriteData:totalBytesWritten:totalBytesExpectedToWrite:))).map { parameters in
             return (parameters[0] as! URLSession,
                     parameters[1] as! URLSessionDownloadTask,
-                    parameters[1] as! Int64,
-                    parameters[1] as! Int64,
-                    parameters[1] as! Int64)
+                    parameters[2] as! Int64,
+                    parameters[3] as! Int64,
+                    parameters[4] as! Int64)
         }
     }
 }
